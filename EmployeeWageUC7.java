d934a23c632b7177ef11cc39067e951f2ffd79fc
Rahul datta <bitrarahuldatta@gmail.com>
2020-09-14 15:07:11 +0530

    Class Method to Compute EmployeeWage

---
+++ EmployeeWageUC7.java
@@ -0,0 +1,39 @@
+public class EmployeeWageUC7 {
+        public static final int isPartTime=1;
+        public static final int isFullTime=2;
+        public static final int EmpRatePerHour=20;
+        public static final int NumOfWorkingDays=20;
+        public static final int MaxHrsInMonth=100;
+
+        public static int computeEmpWage() {
+
+                int empHrs=0;
+                int totalEmpHrs=0;
+                int totalWorkingDays=0;
+
+                while (totalEmpHrs <= MaxHrsInMonth && totalWorkingDays < NumOfWorkingDays) {
+                        totalWorkingDays++;
+                        int empCheck=(int)Math.floor(Math.random()*10)%3;
+
+                        switch (empCheck) {
+                                case isPartTime:
+                                        empHrs=8;
+                                        break;
+                                case isFullTime:
+                                        empHrs=16;
+                                        break;
+                                default:
+                                        empHrs=0;
+                        }
+                        totalEmpHrs+=empHrs;
+                        System.out.println("Day#: "+totalWorkingDays+ "Emp Hrs: "+empHrs);
+                }
+                int totalEmpWage=totalEmpHrs*EmpRatePerHour;
+                System.out.println("Total Emp Wage: "+totalEmpWage);
+                return totalEmpWage;
+        }
+
+        public static void main(String[] args) {
+                computeEmpWage();
+        }
+}
